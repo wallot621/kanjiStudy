aa5d0a10b1352fb5a5ac8d9897674626336142b1
wallot621 <q56026467@gmail.com>
2026-03-25 14:42:01 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-function setAppHeight() {
-    const vh = window.innerHeight;
-    document.documentElement.style.setProperty('--app-height', `${vh}px`);
-}
-
-window.addEventListener('resize', setAppHeight);
-setAppHeight();
-
 let mode = "kanji";
 let tempMode = null;
 let index = 0;
@@ -165,15 +157,34 @@ function update(){
     }
 
     else if(tempMode === "reading"){
-        display.innerHTML = wrap(`
-            <div style="display:flex; flex-direction:column; gap:0.3em; font-size:2em;">
-                <div>${toHiragana(item.on)}</div>
-                <div>${toHiragana(item.kun)}</div>
+    display.innerHTML = wrap(`
+        <div class="reading-layout">
+
+            <div class="reading-kanji">
+                ${item.k}
             </div>
-        `);
-        display.className = "display reading";
-        btnReading.classList.add("active");
-    }
+
+            <div class="reading-grid">
+
+                <div class="label">음</div>
+                <div class="label">훈</div>
+
+                <div class="reading-on">
+                    ${toHiragana(item.on)}
+                </div>
+
+                <div class="reading-kun">
+                    ${toHiragana(item.kun)}
+                </div>
+
+            </div>
+
+        </div>
+    `);
+
+    display.className = "display reading";
+    btnReading.classList.add("active");
+}
 
     else if(tempMode === "words"){
         const m1Size = getMeaningFontSize(item.words[0].m);
